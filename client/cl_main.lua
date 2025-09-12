479d1987bcd9fe2165a96f64cbcd2aaf4590764a
The Ghoul <98289302+lekedog@users.noreply.github.com>
2025-09-12 05:05:50 -0500

    Refactor prop placement and prompt logic
    Signed-off-by: The Ghoul <98289302+lekedog@users.noreply.github.com>

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,79 +1,50 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 local Props = {}
+local SpawnedProps = {}
 local PromptPlacerGroup = GetRandomIntInRange(0, 0xffffff)
 local CancelPrompt, SetPrompt, RotateLeftPrompt, RotateRightPrompt
 
+-- Utility: Register prompts
+local function RegisterPrompt(name, control, holdMode, standardMode)
+    Citizen.CreateThread(function()
+        local prompt = PromptRegisterBegin()
+        PromptSetControlAction(prompt, control)
+        local str = CreateVarString(10, 'LITERAL_STRING', name)
+        PromptSetText(prompt, str)
+        PromptSetEnabled(prompt, true)
+        PromptSetVisible(prompt, true)
+        if holdMode then PromptSetHoldMode(prompt, true) end
+        if standardMode then PromptSetStandardMode(prompt, true) end
+        PromptSetGroup(prompt, PromptPlacerGroup)
+        PromptRegisterEnd(prompt)
+        if name == "Cancel" then CancelPrompt = prompt
+        elseif name == "Set" then SetPrompt = prompt
+        elseif name == "Rotate Left" then RotateLeftPrompt = prompt
+        elseif name == "Rotate Right" then RotateRightPrompt = prompt
+        end
+    end)
+end
+
 Citizen.CreateThread(function()
-    Set()
-    Del()
-    RotateLeft()
-    RotateRight()
+    RegisterPrompt('Set', 0x07CE1E61, true, false)
+    RegisterPrompt('Cancel', 0xF84FA74F, true, false)
+    RegisterPrompt('Rotate Left', 0xA65EBAB4, false, true)
+    RegisterPrompt('Rotate Right', 0xDEB34313, false, true)
 end)
 
-function Del()
-    Citizen.CreateThread(function()
-        local str = 'Cancel'
-        CancelPrompt = PromptRegisterBegin()
-        PromptSetControlAction(CancelPrompt, 0xF84FA74F)
-        str = CreateVarString(10, 'LITERAL_STRING', str)
-        PromptSetText(CancelPrompt, str)
-        PromptSetEnabled(CancelPrompt, true)
-        PromptSetVisible(CancelPrompt, true)
-        PromptSetHoldMode(CancelPrompt, true)
-        PromptSetGroup(CancelPrompt, PromptPlacerGroup)
-        PromptRegisterEnd(CancelPrompt)
-    end)
-end
-
-function Set()
-    Citizen.CreateThread(function()
-        local str = 'Set'
-        SetPrompt = PromptRegisterBegin()
-        PromptSetControlAction(SetPrompt, 0x07CE1E61)
-        str = CreateVarString(10, 'LITERAL_STRING', str)
-        PromptSetText(SetPrompt, str)
-        PromptSetEnabled(SetPrompt, true)
-        PromptSetVisible(SetPrompt, true)
-        PromptSetHoldMode(SetPrompt, true)
-        PromptSetGroup(SetPrompt, PromptPlacerGroup)
-        PromptRegisterEnd(SetPrompt)
-    end)
-end
-
-function RotateLeft()
-    Citizen.CreateThread(function()
-        local str = 'Rotate Left'
-        RotateLeftPrompt = PromptRegisterBegin()
-        PromptSetControlAction(RotateLeftPrompt, 0xA65EBAB4)
-        str = CreateVarString(10, 'LITERAL_STRING', str)
-        PromptSetText(RotateLeftPrompt, str)
-        PromptSetEnabled(RotateLeftPrompt, true)
-        PromptSetVisible(RotateLeftPrompt, true)
-        PromptSetStandardMode(RotateLeftPrompt, true)
-        PromptSetGroup(RotateLeftPrompt, PromptPlacerGroup)
-        PromptRegisterEnd(RotateLeftPrompt)
-    end)
-end
-
-function RotateRight()
-    Citizen.CreateThread(function()
-        local str = 'Rotate Right'
-        RotateRightPrompt = PromptRegisterBegin()
-        PromptSetControlAction(RotateRightPrompt, 0xDEB34313)
-        str = CreateVarString(10, 'LITERAL_STRING', str)
-        PromptSetText(RotateRightPrompt, str)
-        PromptSetEnabled(RotateRightPrompt, true)
-        PromptSetVisible(RotateRightPrompt, true)
-        PromptSetStandardMode(RotateRightPrompt, true)
-        PromptSetGroup(RotateRightPrompt, PromptPlacerGroup)
-        PromptRegisterEnd(RotateRightPrompt)
-    end)
-end
-
-function modelrequest(model)
-    Citizen.CreateThread(function()
-        RequestModel(model)
-    end)
+local function RequestAndLoadModel(model)
+    RequestModel(model)
+    local timeout = 10000
+    local waited = 0
+    while not HasModelLoaded(model) do
+        Wait(50)
+        waited = waited + 50
+        if waited >= timeout then
+            print('[ds-propplacer] Model load timeout:', model)
+            return false
+        end
+    end
+    return true
 end
 
 function PropPlacer(proptype, ObjectModel)
@@ -82,19 +53,18 @@ function PropPlacer(proptype, ObjectModel)
     local pos = GetEntityCoords(myPed)
     local PropHash = GetHashKey(ObjectModel)
     local coords = GetEntityCoords(myPed)
-    local _x,_y,_z = table.unpack(coords)
+    local _x, _y, _z = table.unpack(coords)
     local forward = GetEntityForwardVector(myPed)
     local x, y, z = table.unpack(pos - forward * -2.0)
-    local ox = x -_x
-    local oy = y-_y
-    local oz = z - _z
+    local ox, oy, oz = x - _x, y - _y, z - _z
     local heading = 0.0
 
     SetCurrentPedWeapon(myPed, -1569615261, true)
-    RequestModel(PropHash)
-    while not HasModelLoaded(PropHash) do
-        Wait(100)
+    if not RequestAndLoadModel(PropHash) then
+        exports['ox_lib']:notify({title = 'Could not load model: '..ObjectModel, type = 'error', duration = 5000})
+        return
     end
+
     local tempObj = CreateObject(PropHash, pos.x, pos.y, pos.z, false, false, false)
     local tempObj2 = CreateObject(PropHash, pos.x, pos.y, pos.z, false, false, false)
     AttachEntityToEntity(tempObj2, myPed, 0, ox, oy, 0.5, 0.0, 0.0, 0, true, false, false, false, false)
@@ -103,14 +73,14 @@ function PropPlacer(proptype, ObjectModel)
 
     while true do
         Wait(5)
-        local PropPlacerGroupName  = CreateVarString(10, 'LITERAL_STRING', "PropPlacer")
+        local PropPlacerGroupName = CreateVarString(10, 'LITERAL_STRING', "PropPlacer")
         PromptSetActiveGroupThisFrame(PromptPlacerGroup, PropPlacerGroupName)
-
         AttachEntityToEntity(tempObj, myPed, 0, ox, oy, -0.8, 0.0, 0.0, heading, true, false, false, false, false)
-        if IsControlPressed( 1, 0xA65EBAB4) then
+
+        if IsControlPressed(1, 0xA65EBAB4) then
             heading = heading - 1
         end
-        if IsControlPressed( 1, 0xDEB34313) then
+        if IsControlPressed(1, 0xDEB34313) then
             heading = heading + 1
         end
 
@@ -123,9 +93,6 @@ function PropPlacer(proptype, ObjectModel)
             DeleteEntity(tempObj)
             FreezeEntityPosition(myPed, false)
             break
-RegisterNetEvent('ds-propplacer:client:propPlaced', function(propId)
-    exports['ox_lib']:notify({title = 'Prop placed! ID: '..propId, type = 'success', duration = 7000})
-end)
         end
 
         if PromptHasHoldModeCompleted(CancelPrompt) then
@@ -137,50 +104,53 @@ end)
     end
 end
 
-
-RegisterCommand('removeprop', function(source, args, rawCommand)
-    local propid = tonumber(args[1])
-    if propid then
-        if Props[propid] then
-            TriggerServerEvent('ds-propplacer:server:removeProp', propid)
-            exports['ox_lib']:notify({title = 'Prop deleted: '..propid, type = 'success', duration = 5000})
-        else
-            exports['ox_lib']:notify({title = 'Invalid prop ID: '..tostring(propid), type = 'error', duration = 5000})
-        end
-    else
-    exports['ox_lib']:notify({title = 'Usage: /removeprop [propid]', type = 'error', duration = 5000})
-    end
+RegisterNetEvent('ds-propplacer:client:propPlaced', function(propId)
+    exports['ox_lib']:notify({title = 'Prop placed! ID: '..tostring(propId), type = 'success', duration = 7000})
 end)
-
-RegisterCommand('placeprop', function(source, args, rawCommand)
+--------------------------------------------------------
+--place prop command
+--------------------------------------------------------
+RegisterCommand('placeprop', function(_, args)
     local proptype = args[1] or 'default'
     local model = args[2]
     if not model then
-        print('Usage: /placeprop [type] [model]')
+        exports['ox_lib']:notify({title = 'Usage: /placeprop [type] [model]', type = 'error', duration = 5000})
         return
     end
     PropPlacer(proptype, model)
 end)
-
-local SpawnedProps = {}
+--------------------------------------------------------
+--remove prop command
+--------------------------------------------------------
+RegisterCommand('removeprop', function(source, args, rawCommand)
+    local propid = args[1]
+    if propid then
+        TriggerServerEvent('ds-propplacer:server:removeProp', propid)
+    else
+        exports['ox_lib']:notify({title = 'Usage: /removeprop [propid]', type = 'error', duration = 5000})
+    end
+end)
+--------------------------------------------------------
+--list props command
+--------------------------------------------------------
+RegisterCommand('listprops', function()
+    for id, propData in pairs(Props) do
+        print('Prop ID:', id, 'Type:', propData.proptype, 'Model:', propData.hash or propData.model)
+    end
+end)
 
 RegisterNetEvent('ds-propplacer:client:updatePropData', function(props)
     Props = props
-    -- Remove all currently spawned props
     for id, obj in pairs(SpawnedProps) do
         if DoesEntityExist(obj) then
             DeleteEntity(obj)
         end
         SpawnedProps[id] = nil
     end
-    -- Spawn all props from Props table
     for id, propData in pairs(Props) do
+        if type(propData) == "boolean" then goto continue end
         local model = propData.hash or propData.model
-        if model then
-            RequestModel(model)
-            while not HasModelLoaded(model) do
-                Wait(10)
-            end
+        if model and RequestAndLoadModel(model) then
             local groundZ = propData.z
             local found, z = GetGroundZFor_3dCoord(propData.x, propData.y, propData.z)
             if found then groundZ = z end
@@ -188,6 +158,7 @@ RegisterNetEvent('ds-propplacer:client:updatePropData', function(props)
             SetEntityHeading(obj, propData.h or 0.0)
             SpawnedProps[id] = obj
         end
+        ::continue::
     end
 end)
 
@@ -196,10 +167,5 @@ RegisterNetEvent('ds-propplacer:client:removePropObject', function(propid)
     if SpawnedProps[propid] and DoesEntityExist(SpawnedProps[propid]) then
         DeleteEntity(SpawnedProps[propid])
         SpawnedProps[propid] = nil
-    end
-end)
-RegisterCommand('listprops', function()
-    for id, propData in pairs(Props) do
-        print('Prop ID:', id, 'Type:', propData.proptype, 'Model:', propData.hash or propData.model)
     end
 end)
