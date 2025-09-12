9feea98c6f187d8d7aeeb9efbdbbdf572b8fee24
The Ghoul <98289302+lekedog@users.noreply.github.com>
2025-09-12 03:13:05 -0500

    Refactor prop placement permission check and SQL insert
    Signed-off-by: The Ghoul <98289302+lekedog@users.noreply.github.com>

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -22,16 +22,23 @@ AddEventHandler('ds-propplacer:server:getProps', function()
 end)
 
 -- Place new prop
+local function isOwner(Player)
+    return Player and Player.PlayerData.citizenid == SERVER_OWNER_CITIZENID
+end
+
 RegisterServerEvent('ds-propplacer:server:newProp')
 AddEventHandler('ds-propplacer:server:newProp', function(proptype, position, heading, hash)
     local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
-    if not Player or Player.PlayerData.citizenid ~= SERVER_OWNER_CITIZENID then
-        TriggerClientEvent('ox_lib:notify', src, {title = 'No permission to place props.', type = 'error', duration = 7000 })
+    if not isOwner(Player) then
+        TriggerClientEvent('ox_lib:notify', src, {title = 'No permission to place props.', type = 'error', duration = 7000})
         return
     end
+
+    -- Generate unique propId (preferably with DB or UUID)
     local propId = math.random(111111, 999999)
-    local citizenid = Player.PlayerData.citizenid
+    -- TODO: Replace with DB-generated ID or UUID
+
     local PropData = {
         id = propId,
         proptype = proptype,
@@ -40,28 +47,28 @@ AddEventHandler('ds-propplacer:server:newProp', function(proptype, position, hea
         z = position.z,
         h = heading,
         hash = hash,
-        builder = citizenid,
+        builder = Player.PlayerData.citizenid,
         buildttime = os.time()
     }
+
     Props[propId] = PropData
+
     local insertData = {
         ['@properties'] = json.encode(PropData),
         ['@propid'] = propId,
-        ['@citizenid'] = citizenid,
+        ['@citizenid'] = Player.PlayerData.citizenid,
         ['@proptype'] = proptype,
     }
-    print('[ds-propplacer] SQL Query: INSERT INTO ds_props (properties, propid, citizenid, proptype) VALUES (@properties, @propid, @citizenid, @proptype)')
-    print('[ds-propplacer] Parameters:', json.encode(insertData))
-    -- Check for duplicate propid before insert
-    exports.oxmysql:fetch('SELECT propid FROM ds_props WHERE propid = @propid', {['@propid'] = propId}, function(rows)
-        if rows and #rows > 0 then
-            print('[ds-propplacer] Duplicate propid detected:', propId)
+
+    -- SQL: Rely on DB constraints for uniqueness
+    exports.oxmysql:execute('INSERT INTO ds_props (properties, propid, citizenid, proptype) VALUES (@properties, @propid, @citizenid, @proptype)', insertData, function(result)
+        if result then
+            print('[ds-propplacer] Insert result:', result)
         else
-            exports.oxmysql:execute('INSERT INTO ds_props (properties, propid, citizenid, proptype) VALUES (@properties, @propid, @citizenid, @proptype)', insertData, function(result)
-                print('[ds-propplacer] Insert result:', result)
-            end)
+            print('[ds-propplacer] Error inserting prop:', propId)
         end
     end)
+
     TriggerClientEvent('ds-propplacer:client:propPlaced', src, propId)
     TriggerClientEvent('ox_lib:notify', src, {title = 'Prop Placed! ID: '..propId, type = 'success', duration = 7000})
     TriggerClientEvent('ds-propplacer:client:updatePropData', -1, Props)
