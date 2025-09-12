76a2facd23768457c3ec1ef3cf991f88f6c33663
The Ghoul <98289302+lekedog@users.noreply.github.com>
2025-09-12 05:09:57 -0500

    Modify ds_props table to use IF NOT EXISTS and AUTO_INCREMENT
    Signed-off-by: The Ghoul <98289302+lekedog@users.noreply.github.com>

--- ds_props table(.sql
+++ ds_props table(.sql
@@ -1,5 +1,5 @@
-CREATE TABLE ds_props (
-    propid INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS ds_props (
+    propid INT AUTO_INCREMENT PRIMARY KEY,
     properties TEXT NOT NULL,
     citizenid VARCHAR(50) NOT NULL,
     proptype VARCHAR(50) NOT NULL
